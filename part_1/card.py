b1b2bd7d1f246ae1e864745b45d690d8f8092aa3
James Dickson <jamesstuartdickson@gmail.com>
2023-07-21 17:20:27 +0100

    copied over files and made comments to part 1 code

---
+++ part_1/card.py
@@ -0,0 +1,30 @@
+class CardGame:
+
+
+  def check_for_ace(self, card):
+    # Should be card.value == 1 to check for equality
+    if card.value = 1:
+      return True
+    # Needs a colon at the end of else - should be else:
+    else
+      return False
+   
+
+# Typo here, dif should be def. Also missing a comma between card1 and card 2 - i.e. (self, card1, card2)
+  dif highest_card(self, card1 card2):
+    # line below needs an indent following the def function above. This line has the correct indentation that the below line should have.
+  if card1.value > card2.value:
+    # should return card1 and not just "card", as "card" is not defined in the above method.
+    return card
+  else:
+    return card2
+  
+
+
+def cards_total(self, cards):
+  # "total" is undefined, needs a starting value (0 makes the most sense here) i.e. total = 0
+  total
+  for card in cards:
+    total += card.value
+    return "You have a total of" + total
+  
