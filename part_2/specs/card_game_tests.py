b65d8f08a5f937e216840f93e896eda863b8e361
James Dickson <jamesstuartdickson@gmail.com>
2023-07-21 18:27:16 +0100

    fixed part 2 code

--- part_2/specs/card_game_tests.py
+++ part_2/specs/card_game_tests.py
@@ -2,5 +2,13 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    
+    def setUp(self):
+        card1 = Card("Clubs", 1)
+        card2 = Card("Hearts", 7)
+        card3 = Card("Diamonds", 8)
+    
+    def test_check_for_ace(self):
+        test_result = check_for_ace(card1)
+        self.assertEqual(True, test_result)
